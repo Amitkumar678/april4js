cb5c714600f2e705469f2dd25d4147e17183cdd7
amitkumar <realamit678@gmail.com>
2022-04-07 11:42:59 +0530

    dom

---
+++ main.js
@@ -0,0 +1,20 @@
+// const result=document.getElementsByTagName('h1')
+// console.log(result[0].innerText);
+
+// const result= document.getElementsByClassName('abc')
+// console.log(result[0].innerText);
+
+// function clickme(){
+//     const result=document.getElementsByTagName('h1')
+//     result[4].innerText='ELEVATION ACADEMY'
+// }
+
+// function abc(){
+//     const result=document.getElementById('as')
+//     alert(result.innerHTML)
+// }abc()
+
+// const result=document.createElement('h1')
+// result.setAttribute("class","ipl")
+// result.innerText='hello lucknow'
+// console.log(result);
